aef7877ec97c11783a0df7a72d94fb7dab41fe96
kntjrld <68082685+kntjrld@users.noreply.github.com>
2022-06-29 08:15:06 +0800

    Updated

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -5,6 +5,22 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+
+        
+        if(this.window = '.skills'){
+            $('.html').toggleClass("htmlstart");
+            $('.css').toggleClass("cssstart");
+            $('.Javascript').toggleClass("javascriptstart");
+            $('.jquery').toggleClass("jquerystart");
+            $('.Sqlite').toggleClass("sqlitestart");
+            $('.Microsoft').toggleClass("microsoftstart");
+            $('.Access').toggleClass("accessstart");
+            $('.Java').toggleClass("javastart");
+            $('.NET').toggleClass("netstart");
+            $('.C').toggleClass("cstart");
+            $('.Python').toggleClass("pythonstart");
+            $('.MySql').toggleClass("sqlstart");
+        }
     });
 
     //nav bar script ==> open navigation
